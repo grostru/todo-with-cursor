15f54e70d36e5900032783c2582c2c6dfa7c2707
Gema Rosas Trujillo <gmrosas@knowmadmood.com>
2025-08-14 08:33:34 +0200

    fixed bug next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Tus opciones actuales de Next.js...
+  
+  // Forzar a usar Webpack en lugar de Turbopack
+  webpack: (config) => {
+    return config;
+  },
 };
 
 export default nextConfig;
